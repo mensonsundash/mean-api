c6330d0cda07ed1d2adba4115bbda543d3b98ad2
Menson Sundash <80436970+mensonsundash@users.noreply.github.com>
2024-03-15 16:07:03 +1100

    model schema setup (#3)

---
+++ src/models/role.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { getCollectionName } from "../utils/pluralize.js";
+
+const RoleSchema = new mongoose.Schema({
+    role:{
+        type: String,
+        required: true
+    }
+},{
+    timestamps: true
+});
+
+//generate collection name with prefix and pluralization
+RoleSchema.set('collection', getCollectionName('role'));
+
+const RoleModel = mongoose.model('Role', RoleSchema);
+
+export default RoleModel;
