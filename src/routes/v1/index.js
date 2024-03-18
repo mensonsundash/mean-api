292b6c08a33cb240a5e3bfc9cbd0166e6ef8aac8
menso431 <menson.sundash@gmail.com>
2024-03-18 15:03:19 +1100

    cookie-parser & jwt installed, user with register route added

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import roleRouter from './role.routes.js';
 import authRouter from './auth.routes.js';
+import userRouter from './user.routes.js';
 
 const app = express.Router();
 
+//Router Middleware
 app.use('/role', roleRouter);
 app.use('/auth', authRouter);
+app.use('/user', userRouter);
+
 export default app;
