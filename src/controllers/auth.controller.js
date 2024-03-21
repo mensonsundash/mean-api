45e739878fc6c6d6c0bca4ec899f395009cf9837
menso431 <menson.sundash@gmail.com>
2024-03-21 23:43:27 +1100

    forget-password  route & smtp email sending setup

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,14 @@
 import RoleModel from "../models/role.model.js";
 import UserModel from "../models/user.model.js";
 import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
 import { CreateError, CreateSuccess } from "../utils/responseHandler.js";
+import { signJwt, signJwtRefreshToken } from "../utils/jwt.utils.js";
+import UserTokenModel from "../models/userToken.model.js";
+// import nodemailer from 'nodemailer';
+import globalUrl from "../config/global.config.js";
+import mailSent from "../lib/email/send.js";
+
 
 export async function register(req, res, next) {
     try{
@@ -60,24 +66,113 @@ export async function login(req, res, next) {
         }
 
         const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
-
         if(!isPasswordCorrect){
             return res.status(400).send("Password is incorrect!");
         }
-        const token = jwt.sign(
-            {id: user._id, isAdmin: user.isAdmin, roles: roles},
-            process.env.JWT_SECRET
-        );
+        
+        const accessToken = signJwt({ id: user._id, isAdmin: user.isAdmin, roles: roles });
+       
+        const refreshToken = signJwtRefreshToken({ id: user._id, isAdmin: user.isAdmin, roles: roles });
+        
         // return next(CreateSuccess(200, "Login successfully!"));
-        res.cookie("access_token", token, {httpOnly: true})
+        res.cookie("accessToken", accessToken, {httpOnly: true})
         .status(200)
         .json({
+            accessToken: accessToken,
+            refreshToken: refreshToken,
             status: 200,
             message: "Login successful",
             data: user
         })
     }catch(error){
         // return res.status(500).send("Internal Server Error!");
+        return next(CreateError(500, "Internal Server Error!"));
+    }
+}
+
+
+
+
+// export async function login(req, res, next) {
+//     try{
+//         const user = await UserModel.findOne({email: req.body.email})
+//         .populate("roles", "role");
+
+//         const { roles } = user;
+//         if(!user){
+//             return res.status(404).send("User not found!");
+//         }
+
+//         const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
+
+//         if(!isPasswordCorrect){
+//             return res.status(400).send("Password is incorrect!");
+//         }
+//         const token = jwt.sign(
+//             {id: user._id, isAdmin: user.isAdmin, roles: roles},
+//             process.env.ACCESS_TOKEN_SECRET,
+//             {
+//                 expiresIn: process.env.ACCESS_TOKEN_TTL
+//             }
+//         );
+//         // return next(CreateSuccess(200, "Login successfully!"));
+//         res.cookie("access_token", token, {httpOnly: true})
+//         .status(200)
+//         .json({
+//             accessToken: token,
+//             status: 200,
+//             message: "Login successful",
+//             data: user
+//         })
+//     }catch(error){
+//         // return res.status(500).send("Internal Server Error!");
+//         return next(CreateError(500, "Internal Server Error!"));
+//     }
+// }
+
+export async function forgetPassword(req, res, next) {
+    try{
+        const email = req.body.email;
+        const user = await UserModel.findOne({email: {$regex: '^'+email+'$', $options: 'i'} });
+
+        if(!user){
+            return next(CreateError(404, "User not found"));
+        }
+        
+        const expiryMinutes = 5;
+        const expiryHours = 0;
+        const expiryTimeInMilliseconds = (expiryMinutes * 60 + expiryHours * 60 * 60) * 1000;
+
+        const now = new Date();
+        const expiryDate = new Date(now.getTime() + expiryTimeInMilliseconds);
+
+        const token = signJwt({email: user.email}, expiryTimeInMilliseconds);
+        
+        const newToken = new UserTokenModel({
+            userId: user.id,
+            token: token
+        });
+
+        const web_url = globalUrl.web_url;
+        const resetLink = web_url +`/auth/reset-password?token=${newToken.token}`;
+        
+        mailSent({
+                to: user.email,
+                subject: 'Reset your password',
+                template: 'reset-password',
+                templateVars: {
+                    emailAddress: user.email,
+                    resetLink,
+                    resetTokenExpires: expiryDate,
+                    firstName: user.firstName
+                },
+                newToken
+            });
+        
+
+        return next(CreateSuccess(200, 'Mail sent', resetLink));
+    
+    }catch(error){
         return next(CreateError(500, "Internal Server Error!"));
     }
 }
--- src/routes/v1/auth.routes.js
+++ src/routes/v1/auth.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { login, register, registerAdmin } from '../../controllers/auth.controller.js';
+import { forgetPassword, login, register, registerAdmin } from '../../controllers/auth.controller.js';
 const app = express.Router();
 
 app.post("/register", register);
 app.post("/login", login);
 app.post("/register-admin", registerAdmin);
+app.post("/forget-password", forgetPassword)
 export default app;
