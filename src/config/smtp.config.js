656315524c4844babcb83d62163db2cd69d20458
menso431 <menson.sundash@gmail.com>
2024-03-25 15:03:01 +1100

    smtp-config

--- src/config/smtp.config.js
+++ src/config/smtp.config.js
@@ -1,16 +1,18 @@
 /**
  * SendInBlue Account for SMTP
  * */
+import dotenv from 'dotenv';
+dotenv.config();
 
 const smtpConfig = {
     smtp: {
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
-        username: process.env.SMTP_LOGIN_USERNAME,
-        password: process.env.SMTP_LOGIN_PASSWORD
+        username: process.env.SMTP_USERNAME,
+        password: process.env.SMTP_PASSWORD
     },
     support: {
-        email: process.env.SMTP_LOGIN_USERNAME,
+        email: process.env.SMTP_USERNAME,
         name: 'MEAN Admin'
     }
 };
