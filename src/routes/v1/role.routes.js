a46f3230c777a324851ff19d064b4ececab93a83
Menson Sundash <80436970+mensonsundash@users.noreply.github.com>
2024-03-26 15:48:14 +1100

    server changes (#30)

--- src/routes/v1/role.routes.js
+++ src/routes/v1/role.routes.js
@@ -4,8 +4,8 @@ const app = express.Router();
 import { createRole, deleteRole, getAllRoles, updateRole } from '../../controllers/role.controller.js';
 import { verifyAdmin } from '../../middlewares/verifyToken.js';
 
-app.get('/', verifyAdmin, getAllRoles)
-app.post('/', verifyAdmin, createRole);
+app.get('/', getAllRoles)
+app.post('/', createRole);
 app.put('/update/:id', verifyAdmin, updateRole)
 app.delete('/:id', verifyAdmin, deleteRole)
 
